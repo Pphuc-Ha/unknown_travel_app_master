b645359661b83e297cc620895d971ddd480bc8a2
Pphuc-Ha <55459550+Pphuc-Ha@users.noreply.github.com>
2019-11-10 12:29:38 +0700

    Layout của Login và SignUp

---
+++ TravelApp/app/src/main/java/com/example/travelapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.travelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin, btnSignup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnSignup = (Button)findViewById(R.id.btnSignup);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
